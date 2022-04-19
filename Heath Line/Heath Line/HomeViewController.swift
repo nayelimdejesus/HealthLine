9e752c5b19d33e789c02307b11009682ec574c21
Nayeli De Jesus <nayelidjm142@gmail.com>
2022-04-19 13:04:54 -0700

    Updated Home page

--- Heath Line/Heath Line/HomeViewController.swift
+++ Heath Line/Heath Line/HomeViewController.swift
@@ -9,12 +9,12 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-    
+
 
     /*
     // MARK: - Navigation
